d4f5698aa9bf834bd97d34e6f4019e0e2e5de4bf
Ying Rong <ying9876543210@hotmail.com>
2022-04-22 19:11:56 -0700

    Almost done TAT

--- flask_app/controllers/orders.py
+++ flask_app/controllers/orders.py
@@ -2,13 +2,46 @@ from flask_app import app
 from flask import Flask, render_template, request, redirect, session, request, flash
 from flask_bcrypt import Bcrypt
 from flask_app.models.user import User
-from flask_app.models.order import Pizza
+from flask_app.models.order import Pizza, Order
 
 @app.route('/customize')         
 def customize():
     if 'users_id' not in session:
         return redirect('/logout')
-    data ={
+    user_in_session ={
         'id': session['users_id']
     }    
-    return render_template ("customize.html", user=User.get_user_by_id(data))
+    return render_template ("customize.html", user=User.get_user_by_id(user_in_session))
+
+
+
+@app.route('/customizepizza', methods=['POST'])         
+def customizepizza():
+    pizza = {
+        "size":request.form['size'],
+        "name":request.form['name'],
+        "crust": request.form['crust'],
+        "cheese": request.form['cheese'],
+        "topping1": request.form['topping1'],
+        "topping2": request.form['topping2'],
+        "topping3": request.form['topping3'],
+    }
+    Pizza.save(pizza)
+    return redirect("/add_to_order")
+
+@app.route('/add_to_order') 
+def add_to_order():
+    user_in_session ={
+        'id': session['users_id']
+    }   
+    return render_template ("orders.html", user=User.get_user_by_id(user_in_session),pizzas=Pizza.get_one(Pizza))
+
+
+# @app.route('/place_order',methods=['POST']) 
+# def place_order():
+#     data = {
+#         "user_id":session['users_id'],
+#         "total": request.form['total']
+#     }
+#     Order.save(data)
+#     return redirect("/check_out")
--- flask_app/models/order.py
+++ flask_app/models/order.py
@@ -16,5 +16,43 @@ class Pizza:
         
     @classmethod
     def save(cls, data):
-        query = 'INSERT INTO pizza (name,size,crust,cheese,topping1, topping2,topping3,price) VALUES (%(name)s,%(size)s,%(crust)s,%(cheese)s,%(topping1)s,%(topping2)s,%(topping3)s,%(price)s );'
+        query = 'INSERT INTO pizzas (name,size,crust,cheese,topping1, topping2,topping3) VALUES (%(name)s,%(size)s,%(crust)s,%(cheese)s,%(topping1)s,%(topping2)s,%(topping3)s);'        
+        return connectToMySQL(cls.db).query_db(query,data)
+    
+    def get_total(cls):
+        query = 'Select  (sizes.price + topping1.price + topping2.price + topping3.price) as total from pizzas \
+                left outer join toppings as topping1 on topping1 = topping1.name  \
+                left outer join toppings as topping2 on topping2 = topping2.name \
+                left outer join toppings as topping3 on topping3 = topping3.name\
+                left outer join sizes on pizzas.size = sizes.size\
+                where id = (SELECT MAX(ID) FROM pizzas)'
+        total = connectToMySQL(cls.db).query_db(query)
+        return total
+    
+    # def update_price(cls,total):
+    #     query = "UPDATE pizzas SET price= %(total)s where id = (SELECT MAX(ID) FROM pizzas);"
+    #     return connectToMySQL(cls.db).query_db(query,total)    
+    
+    def get_one (cls):
+        query = "Select pizzas.size as size, cheese, crust, topping1,  topping2, topping3,  (sizes.price + topping1.price + topping2.price + topping3.price) as total from pizzas \
+                left outer join toppings as topping1 on topping1 = topping1.name  \
+                left outer join toppings as topping2 on topping2 = topping2.name \
+                left outer join toppings as topping3 on topping3 = topping3.name\
+                left outer join sizes on pizzas.size = sizes.size\
+                where id = (SELECT MAX(ID) FROM pizzas)"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        pizza = connectToMySQL(cls.db).query_db(query)
+        return pizza
+    
+class Order:
+    db = "pizza"
+    def __init__( self , data ):
+        self.id = data['id']
+        self.user_id = data['users_id']
+        self.price = data['total_price'] 
+    
+    @classmethod    
+    def save(cls, data):
+        query = 'INSERT INTO pizzas (user_id,total_price) VALUES (%(user_id)s,%(total)s);'        
         return connectToMySQL(cls.db).query_db(query,data)    
+        
